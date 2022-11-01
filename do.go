2a9d2d3490d48782a24bd6a3d8b804c85ed0fcf5
Trent Slutzky <trent.slutzky@gmail.com>
2022-11-01 11:42:02 -0400

    implement editing

--- do.go
+++ do.go
@@ -25,6 +25,7 @@ type model struct {
   deleting           bool
   item_to_delete     int
   editing            bool
+  item_to_edit       int
   creating           bool
   show_help          bool
   new_item_textInput textinput.Model
@@ -38,11 +39,15 @@ var plus_style = lipgloss.NewStyle().
   Bold(true).
   Foreground(lipgloss.Color("5"))
 
+var edit_style = lipgloss.NewStyle().
+  Bold(true).
+  Foreground(lipgloss.Color("3"))
+
 const (
   plus_icon = " "
   uncheck_icon = ""
   check_icon = ""
-  edit_icon = "ﯽ"
+  edit_icon = " "
   delete_icon = ""
 )
 
@@ -66,6 +71,8 @@ func initialModel() model {
     editing: false,
     creating: false,
     show_help: true,
+    item_to_edit: -1,
+    item_to_delete: -1,
     new_item_textInput: ti,
   }
 }
@@ -99,6 +106,13 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
     case "q":
       if is_not_editing {
+        var home = os.Getenv("HOME")
+        var filename = home+"/.local/share/do/items.json"
+        new_items := TodoListItems{
+          Items: m.items,
+        }
+        file, _ := json.MarshalIndent(new_items, "", " ")
+        _ = ioutil.WriteFile(filename,file, 0644)
         return m, tea.Quit
       }
 
@@ -114,12 +128,25 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
     case "n":
       if is_not_editing {
+        m.new_item_textInput.Prompt = plus_style.Render(" "+plus_icon+" ")
         m.creating = true
       }
 
     case "e":
-      if is_not_editing {
+      if is_not_editing && len(m.items) > 0 {
         m.editing = true
+        m.new_item_textInput.Prompt = edit_style.Render(" "+edit_icon+" ")
+        m.new_item_textInput.SetValue(m.items[m.cursor].Title)
+      }
+
+    case "G":
+      if is_not_editing && len(m.items) > 0 {
+        m.cursor = len(m.items) - 1
+      }
+
+    case "g":
+      if is_not_editing && len(m.items) > 0 {
+        m.cursor = 0
       }
 
     case "d":
@@ -145,6 +172,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
         m.creating = false
         m.editing = false
         m.deleting = false
+        m.item_to_delete = -1
+        m.item_to_edit = -1
         m.new_item_textInput.Blur()
         _ = m.new_item_textInput.Reset()
       }
@@ -155,20 +184,28 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
       }
 
     case "enter":
-      if m.creating && len(m.new_item_textInput.Value()) > 0 {
-        new_item := TodoListItem{
-          Title: m.new_item_textInput.Value(),
-          Done: false,
-        }
-        if len(m.items) > 0 {
-          m.items = insert(m.items, m.cursor + 1, new_item)
-          m.cursor++
-        } else {
-          m.items = append(m.items, new_item)
-        }
+      if m.editing {
+        m.items[m.cursor].Title = m.new_item_textInput.Value()
         m.new_item_textInput.Blur()
         _ = m.new_item_textInput.Reset()
-        m.creating = false
+        m.editing = false
+        m.item_to_edit = -1
+      } else if m.creating {
+        if len(m.new_item_textInput.Value()) > 0 {
+          new_item := TodoListItem{
+            Title: m.new_item_textInput.Value(),
+            Done: false,
+          }
+          if len(m.items) > 0 {
+            m.items = insert(m.items, m.cursor + 1, new_item)
+            m.cursor++
+          } else {
+            m.items = append(m.items, new_item)
+          }
+          m.new_item_textInput.Blur()
+          _ = m.new_item_textInput.Reset()
+          m.creating = false
+        }
       } else {
         if is_not_editing && len(m.items) > 0 {
           m.items[m.cursor].Done = !m.items[m.cursor].Done
@@ -177,7 +214,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     }
   }
 
-  if m.creating == true {
+  if m.editing && m.item_to_edit == -1 {
+    m.item_to_edit = m.cursor
+    m.new_item_textInput.Update(m.items[m.cursor].Title)
+  }
+
+  if m.creating == true || m.editing == true {
     if m.new_item_textInput.Focused() == true {
       m.new_item_textInput, cmd = m.new_item_textInput.Update(msg)
     } else {
@@ -212,6 +254,14 @@ var title_style_delete = lipgloss.NewStyle().
   MarginBottom(1).
   MarginLeft(0)
 
+var title_style_edit = lipgloss.NewStyle().
+  Bold(true).
+  Foreground(lipgloss.Color("0")).
+  Background(lipgloss.Color("3")).
+  MarginTop(0).
+  MarginBottom(1).
+  MarginLeft(0)
+
 var normal_style = lipgloss.NewStyle().
   Bold(false).
   Foreground(lipgloss.Color("7"))
@@ -232,6 +282,11 @@ var delete_style = lipgloss.NewStyle().
   Bold(true).
   Foreground(lipgloss.Color("1"))
 
+var edit_text_style = lipgloss.NewStyle().
+  Bold(false).
+  Background(lipgloss.Color("3")).
+  Foreground(lipgloss.Color("1"))
+
 var help_style_normal = lipgloss.NewStyle().
   Foreground(lipgloss.Color("#555"))
 
@@ -248,6 +303,8 @@ func (m model) View() string {
     s += title_style_new_item.Render(header_text)
   } else if m.deleting {
     s += title_style_delete.Render(header_text)
+  } else if m.editing {
+    s += title_style_edit.Render(header_text)
   } else {
     s += title_style_normal.Render(header_text)
   }
@@ -278,14 +335,25 @@ func (m model) View() string {
       checked = delete_style.Render(" " + delete_icon + "  ")
       line_string = delete_style.Render("%s")
     }
-
-    s += fmt.Sprintf(checked)
-    s += fmt.Sprintf(line_string, item.Title)
-    if m.cursor == i && m.creating {
-      s += "\n"
-      s += fmt.Sprintf("%s", m.new_item_textInput.View())
+    if m.editing && m.item_to_edit == i {
+      checked = edit_style.Render(" " + delete_icon + "  ")
+      line_string = edit_text_style.Render("%s")
     }
-    s += "\n"
+
+    if( m.item_to_edit != i ) {
+      s += fmt.Sprintf(checked)
+      s += fmt.Sprintf(line_string, item.Title)
+      s += "\n"
+    }
+    if m.cursor == i && m.creating {
+      s += fmt.Sprintf("%s", m.new_item_textInput.View())
+      s += "\n"
+    }
+    if m.cursor == i && m.editing {
+      m.new_item_textInput.Update(item.Title)
+      s += fmt.Sprintf("%s", m.new_item_textInput.View())
+      s += "\n"
+    }
   }
   
   if len(m.items) == 0 {
@@ -330,6 +398,9 @@ func (m model) View() string {
       s += "\n "
       s += help_style_key.Render(" ")
       s += help_style_normal.Render("toggle")
+      s += "  "
+      s += help_style_key.Render("e ")
+      s += help_style_normal.Render("edit")
       s += "  "
       s += help_style_key.Render("? ")
       s += help_style_normal.Render("toggle help")
